f87566666a851d399052e4483aacf487ae4aa30d
pburkiewicz <burkiewicz.patrycja@gmail.com>
2020-10-13 20:22:25 +0200

    Generate new file from Vertabelo. Add file for answers.

--- Ćwiczenie2/Zadanie5_Cwiczenie2_create_VERTABELO.sql
+++ Ćwiczenie2/Zadanie5_Cwiczenie2_create_VERTABELO.sql
@@ -1,9 +1,9 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2020-10-13 17:02:19.777
+-- Last modification date: 2020-10-13 18:07:30.113
 
 -- tables
 -- Table: Producenci
-CREATE TABLE Producenci (
+CREATE TABLE Sklep.Producenci (
     id_producenta bigserial  NOT NULL,
     nazwa_producenta varchar  NOT NULL,
     mail varchar  NOT NULL,
@@ -11,10 +11,10 @@ CREATE TABLE Producenci (
     CONSTRAINT Producenci_pk PRIMARY KEY (id_producenta)
 );
 
-CREATE INDEX Producenci_idx on Producenci (nazwa_producenta ASC);
+CREATE INDEX Producenci_idx on Sklep.Producenci (nazwa_producenta ASC);
 
 -- Table: Produkty
-CREATE TABLE Produkty (
+CREATE TABLE Sklep.Produkty (
     id_produktu bigserial  NOT NULL,
     nazwa_produktu varchar  NOT NULL,
     cena decimal(10,2)  NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE Produkty (
     CONSTRAINT Produkty_pk PRIMARY KEY (id_produktu)
 );
 
-CREATE INDEX Produkty_idx on Produkty (nazwa_produktu ASC);
+CREATE INDEX Produkty_idx on Sklep.Produkty (nazwa_produktu ASC);
 
 -- Table: Zamowienia
 CREATE TABLE Sklep.Zamowienia (
@@ -34,9 +34,9 @@ CREATE TABLE Sklep.Zamowienia (
 
 -- foreign keys
 -- Reference: Produkty_Producenci (table: Produkty)
-ALTER TABLE Produkty ADD CONSTRAINT Produkty_Producenci
+ALTER TABLE Sklep.Produkty ADD CONSTRAINT Produkty_Producenci
     FOREIGN KEY (Producenci_id_producenta)
-    REFERENCES Producenci (id_producenta)  
+    REFERENCES Sklep.Producenci (id_producenta)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
@@ -44,7 +44,7 @@ ALTER TABLE Produkty ADD CONSTRAINT Produkty_Producenci
 -- Reference: Zamowienia_Produkty (table: Zamowienia)
 ALTER TABLE Sklep.Zamowienia ADD CONSTRAINT Zamowienia_Produkty
     FOREIGN KEY (Produkty_id_produktu)
-    REFERENCES Produkty (id_produktu)  
+    REFERENCES Sklep.Produkty (id_produktu)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
---
+++ Ćwiczenie2/Ćwiczenie2.sql
@@ -0,0 +1,7 @@
+/* 1,2,3,4*/
+/*
+https://my.vertabelo.com/public-model-view/05HxsuJtjpDq8PXBVwNYUP9hh1Hid3UzojX31U0C74MZ0U1yaqBzjg9h5WpRCpU1?x=2403&y=2646&zoom=0.3854
+*/
+
+/* 5 */
+/* W osobnym pliku "Zadanie5_Cwiczenie2_create_VERTABELO.sql" wygenerowanym z użyciem Vertabelo.*/
